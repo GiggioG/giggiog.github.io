d9cc444811cb6a2a0ae5b4be228e12170cb3ccc8
GiggioG <dimitrstanimirov@gmail.com>
2022-07-20 22:46:19 +0300

    fix:localStorage;load faster : removed includesJS

--- res/js/theme.js
+++ res/js/theme.js
@@ -1,25 +1,28 @@
-function themeButton(){
-    let icon = darkTheme?"sun":"moon";
-    let msg = darkTheme?"Light Theme":"Dark Theme";
+function themeButton() {
+    let icon = darkTheme ? "sun" : "moon";
+    let msg = darkTheme ? "Light Theme" : "Dark Theme";
     document.querySelector("nav ul li a#theme").innerHTML = `
         <i class="far fa-${icon} fa-2x"></i>
         <span class="content-paragraph">${msg}</span>
     `;
 }
-function switchTheme(){
+function switchTheme() {
     darkTheme = !darkTheme;
     document.body.classList.toggle("darkTheme");
+    localStorage.setItem("giggiog.github.io_darktheme", darkTheme);
     themeButton();
 }
-document.querySelector("body").onload = ()=>{
+{
     window.darkTheme = JSON.parse(localStorage.getItem("giggiog.github.io_darktheme"));
-    if(darkTheme == null){
+    if (darkTheme == null) {
         localStorage.setItem("giggiog.github.io_darktheme", false);
         darkTheme = false;
     }
-    if(darkTheme){
+    if (darkTheme) {
         document.body.classList.add("darkTheme");
     }
-    themeButton();
+}
+document.querySelector("body").onload = () => {
     document.querySelector("nav ul li a#theme").addEventListener("click", switchTheme);
+    themeButton();
 }
